4e673787ef40766ceb1fba8ef8797d6879cbf2ba
eric <496659850@qq.com>
2019-07-24 09:40:27 +0800

    生产者-消费者

---
+++ src/ConsumeThread.java
@@ -0,0 +1,16 @@
+
+public class ConsumeThread extends Thread {
+    private Consumer c;
+
+    public ConsumeThread(Consumer c) {
+        this.c = c;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            c.consmer();
+        }
+    }
+
+}
